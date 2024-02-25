0eeaf9be004e10b5f14f06c24c7f6ac0dcb6210c
JonesKwameOsei <oseikwamejones@gmail.com>
2024-02-25 18:08:17 +0000

    Remote cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "example-org-9a90ed"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+ */
 
   required_providers {
     aws = {
